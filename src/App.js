ba08adae68447d53fbdadd11abb49abadac1fae3
rizkaumi <93757521+rizkaumi@users.noreply.github.com>
2021-12-10 18:12:26 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import FormProfile from "./component/FormProfile/FormProfile";
+
+
+function App() {
+    
+  return (
+    <div >
+      <FormProfile />
+      
+    </div>
+  );
+}
+
+export default App;
