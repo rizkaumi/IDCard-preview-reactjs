0cffb2d41f17d865f15fa8ffac6e82840de7d1e6
rizkaumi <93757521+rizkaumi@users.noreply.github.com>
2021-12-10 23:36:39 +0700

    Update FormProfile.js

--- src/component/FormProfile/FormProfile.js
+++ src/component/FormProfile/FormProfile.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 import Modal from "react-modal";
 import "./form.css";
 import ModalWindow from "../ModalWindow/ModalWindow";
+import image from '.././image/profil.png'
 
 
 
-function FormProfile(props) {
+function FormProfile() {
   const form ={
-    name:"",
+    fullname:"",
     idnumber:"",
     birthdate:"",
     email:"",
@@ -16,58 +17,47 @@ function FormProfile(props) {
     religion:""
   }
 
-  const [{ name, idnumber, birthdate, email, gender, religion}, setForm] = useState(form);
+  const [file, setFile]= useState(image);
+
+  const [{ fullname, idnumber, birthdate, email, gender, religion}, setForm] = useState(form);
 
   const [data, setData] = useState([]) 
-  const [avatar, setAvatar]= useState([]);
-
-  const avahandle = (event) => {
-    if (event.target.files.length > 0) {
-      let src = URL.createObjectURL(event.target.files[0]);
-      setAvatar(src);
-    }
-  };
-  const handleAva = (event) => {
-    if (event.target.files.length > 0) {
-      let src = URL.createObjectURL(event.target.files[0]);
-      setAvatar({
-        ...avatar,
-        [event.target.name]: src,
-      });
-    }
-  };
-
+  
   const changeHandler = (event) => {
     const {name, value} = (event.target);
     setForm(prevState=> ({...prevState, [name]:value}));
   };
 
+  const avatarchangeHandler = (event) => {
+    let src = setFile(URL.createObjectURL(event.target.files[0]))
+    setForm(prevState => ({ ...prevState, name: src }));
+  }
+
   const submitHandler = (event) => {
     event.preventDefault();
-    setData([...data, {"id":data.length +1, "name":name, "birthdate":birthdate, "idnumber":idnumber, "email":email, "gender":gender, "religion":religion, 'avatar':avatar}])
+    setData([...data, {"id":data.length+1, "fullname":fullname, "idnumber":idnumber, "birthdate":birthdate, "email":email, "gender":gender, "religion":religion, 'file':file}])
     setForm({
-      name: "",
+      fullname: "",
       birthdate: "",
       idnumber: "",
       email: "",
       gender: "",
       religion: "" 
     })
-    setAvatar("")
+    setFile(image);
   }  
   const handleReset = () => {
     Array.from(document.querySelectorAll("input")).forEach(
       input => (input.value = "")
     );
     setData({
-      name: [{}],
-      birthdate: [{}],
+      fullname: [{}],
       idnumber: [{}],
+      birthdate: [{}],
       email: [{}],
       gender: [{}],
       religion: [{}],
-      avatar:[{}] 
-
+      file:[{}]
     });
   };
 
@@ -75,14 +65,14 @@ function FormProfile(props) {
     event.preventDefault();
     setData([])
     setForm({
-      name: "",
-      birthdate: "",
+      fullname: "",
       idnumber: "",
+      birthdate: "",
       email: "",
       gender: "",
       religion: ""
     })
-    
+    setFile(image);
   }
   
   const [modalIsOpen,setModalIsOpen] = useState(false);
@@ -94,7 +84,19 @@ function FormProfile(props) {
   const setModalIsOpenToFalse =()=>{
       setModalIsOpen(false)
   }
-  
+  const modalstyle={
+    content:{
+    position: 'absolute',
+    width: '55%',
+    inset: '40px',
+    background: '#F0AA89',
+    overflow: 'auto',
+    outline: 'none',
+    padding: '10px',
+    marginLeft: '20%'
+    }
+  };
+
   return (
     <>
     <div className="form-wrapper">
@@ -107,8 +109,8 @@ function FormProfile(props) {
             <div className="form-group-box">
                  <div className="form-group">
                  <label className="form-data-label"><b>Name</b>
-                 <input type="text" className="form-data-input" name="name" id="name"
-                  placeholder="Enter your name"
+                 <input type="text" className="form-data-input" name="fullname" id="fullname"
+                  placeholder="Enter your fullname"
                   onChange={changeHandler} 
                   />
                  </label>  
@@ -119,8 +121,8 @@ function FormProfile(props) {
         <div className="form-field-box">
             <div className="form-group-box">
                 <div className="form-group">
-                    <label className="form-data-label"><b>Date of Bith</b>
-                    <input type="date" className="form-data-input" name="datebirth" id="datebirth" onChange={changeHandler}/>
+                    <label className="form-data-label"><b>Date of Birth</b>
+                    <input type="date" className="form-data-input" name="birthdate" id="birthdate" onChange={changeHandler}/>
                     </label> 
                 </div>
             </div>
@@ -162,9 +164,9 @@ function FormProfile(props) {
             <div className="form-group-box">
                 <div className="form-group">
                     <label className="form-data-label"><b>Religion</b></label>
-                    <div class="form-select__group">
-                      <select name="religion" id="religion" class="form-select" onChange={changeHandler}>
-                      <option selected="" value="Default">Choose your religion</option>
+                    <div className="form-select__group">
+                      <select name="religion" id="religion" className="form-select" onChange={changeHandler}>
+                      <option value="Default">Choose your religion</option>
                       <option value="Islam">Islam</option>
                       <option value="Kristen">Kristen</option>
                       <option value="Katolik">Katolik</option>
@@ -193,8 +195,8 @@ function FormProfile(props) {
                 <div className="form-group">
                     <label className="form-data-label"><b>Upload your photo</b></label>
                     <input type="file" className="form-data-input picture-form" name="avatar" 
-                    id="avatar" 
-                    onChange={avahandle} onUpload={handleAva}/>
+                    id="avatar" alt='foto'
+                    onChange={avatarchangeHandler}/>
                 </div>
             </div>
         </div>
@@ -209,15 +211,15 @@ function FormProfile(props) {
           <button className="Previewbutton" id="button" type="button" onClick={setModalIsOpenToTrue}>
             Preview
           </button>
-          <Modal isOpen={modalIsOpen} onRequestClose={()=> setModalIsOpen(false)}>
+          <Modal isOpen={modalIsOpen} style={modalstyle} onRequestClose={()=> setModalIsOpen(false)}>
             <button onClick={setModalIsOpenToFalse} 
             style={{background: "#4d4d4d", border: "none", width: '20px', height:'20px', color: "#ffff", marginTop:"10px", marginLeft:"10px"}}>x</button>
-             <div className="form-field-button">
-          <button className="Resetbutton" type="button" onClick={resetHandler}>
-            Reset
-          </button>
-          </div>
-            <ModalWindow data={data} name={name} birthdate={birthdate} idnumber={idnumber} email={email} gender={gender} religion={religion} avatar={avatar}/>
+            <div className="form-field-button">
+              <button className="Resetbutton" type="button" onClick={resetHandler}>
+               Reset
+              </button>
+           </div>
+            <ModalWindow data={data} fullname={fullname} idnumber={idnumber} birthdate={birthdate} email={email} gender={gender} religion={religion} file={file}/>
             </Modal>
         </div>
         
